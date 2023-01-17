4ef8264e86cf19be464719aea5c6c83cb5b69be6
Bilal Aydin <bilal@arvisgames.com>
2023-01-17 15:39:31 +0300

    *Sample Installer added.

---
+++ Assets/Package/Samples~/HowToUse/SampleInstaller.cs
@@ -0,0 +1,15 @@
+﻿using dynamicscroll;
+using pooling;
+using Zenject;
+
+namespace Arvis.BoardRoyaleDuels
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind(typeof(DynamicScroll<,>)).AsTransient();
+            Container.Bind(typeof(Pooling<>)).AsTransient().WithArguments(Container);
+        }
+    }
+}
